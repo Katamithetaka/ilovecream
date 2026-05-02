39bb1691bc099406a9afcec2e438613b1be5c234
rumi <>
2026-05-02 11:33:05 +0200

    added epilepsy warning

--- refresh.php
+++ refresh.php
@@ -1,8 +1,14 @@
 <?php
+
 ob_start();
 include 'index.php';
 $index = ob_get_clean();
 file_put_contents('index.html', $index);
+
+ob_start();
+include 'web.php';
+$index = ob_get_clean();
+file_put_contents('web.html', $index);
 
 ob_start();
 include 'chat.php';
---
+++ web.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="shortcut icon" href="./assets/images.ico">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="styles.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body  id="background">
+  <header></header>
+  <main>
+
+
+
+
+
+
+
+    <div id="container">
+
+
+      <div id="left">
+
+        <h1 id="title">
+          <img src="assets/exports/hiiiii_flip.gif">
+          HELLOOOO WELCOME TO MY PAGE :D
+          <img src="assets/exports/hiiiii.gif">
+        </h1>
+
+        <p style="color: rgb(25, 25, 25); font-size: 16px; margin-left: 12px;">
+          Hiii! I'm cream, and no, not the rabbit (unfortunately!)<br />
+          I like this rabbit so much that I just wanna be her honestly but since I can't, you get this website instead
+          :D
+          You can chat with me and other people <a href="./chat.html">here</a>!
+        </p>
+
+        <div style="display: flex; padding: 8px; gap: 8px; flex-direction: column;">
+          <div>
+
+            <h1>
+              sprites that i took way too much effort to make them into gifs!
+            </h1>
+
+            <div style="display: flex; padding: 8px; gap: 8px; flex-wrap: wrap;">
+              <img src="assets/exports/dance.gif" alt="Danceing cream!">
+              <img src="assets/exports/dance_flip.gif" alt="Danceing cream flipped!">
+              <img src="assets/exports/fly.gif" alt="Flying cream!">
+              <img src="assets/exports/fly_flip.gif" alt="Flying cream flipped!">
+              <img src="assets/exports/fullspeed.gif" alt="Full speed cream!">
+              <img src="assets/exports/fullspeed_flip.gif" alt="Full speed cream flipped!">
+              <img src="assets/exports/hiiiii.gif" alt="Hiii cream!">
+              <img src="assets/exports/hiiiii_flip.gif" alt="Hiii cream flipped!">
+              <img src="assets/exports/icecream_eat_loop.gif" alt="Eating ice cream!">
+              <img src="assets/exports/icecream_eat_loop_flip.gif" alt="Eating ice cream flipped!">
+              <img src="assets/exports/icecream_full.gif" alt="Full ice cream!">
+              <img src="assets/exports/icecream_full_flip.gif" alt="Full ice cream flipped!">
+              <img src="assets/exports/icecream_loop_1.gif" alt="Ice cream loop 1!">
+              <img src="assets/exports/icecream_loop_1_flip.gif" alt="Ice cream loop 1 flipped!">
+              <img src="assets/exports/idle.gif" alt="Idle cream!">
+              <img src="assets/exports/idle_flip.gif" alt="Idle cream flipped!">
+              <img src="assets/exports/run1.gif" alt="Running cream!">
+              <img src="assets/exports/run1_flip.gif" alt="Running cream flipped!">
+              <img src="assets/exports/run2.gif" alt="Running cream!">
+              <img src="assets/exports/run2_flip.gif" alt="Running cream flipped!">
+              <img src="assets/exports/teabag.gif" alt="Teabag cream!">
+              <img src="assets/exports/teabag_flip.gif" alt="Teabag cream flipped!">
+              <img src="assets/exports/walk1.gif" alt="Walking cream!">
+              <img src="assets/exports/walk1_flip.gif" alt="Walking cream flipped!">
+              <img src="assets/exports/walk2.gif" alt="Walking cream!">
+              <img src="assets/exports/walk2_flip.gif" alt="Walking cream flipped!">
+              <img src="assets/exports/walk2_faster.gif" alt="Faster walking cream!">
+              <img src="assets/exports/walk2_faster_flip.gif" alt="Faster walking cream flipped!">
+            </div>
+          </div>
+        </div>
+            <?php include 'footer.html'; ?>
+
+      </div>
+      <div id="right">
+        <img id="happy" src="assets/happy_new_year.jpg" alt="Happy New Year!">
+
+        <a href="http://www.dragonflycave.com/quizzes/what-pokemon-are-you/"">
+          <img src=" http://www.dragonflycave.com/wpay/togetic.gif" alt="I am a Togetic!"
+          title="Find out what Pokémon you are at The Cave of Dragonflies">
+        </a>
+      </div>
+    </div>
+  </main>
+  <footer></footer>
+</body>
+
+</html>
